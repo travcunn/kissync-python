51f3ea934415c71fb6466f9b5288a780ae7f73f0
Taylor Brazelton <ryanb58@live.com>
2013-04-18 19:40:56 -0400

    Added exit after logout functionality..

--- accountwidget.py
+++ accountwidget.py
@@ -53,7 +53,10 @@ class LogoutLabel(QtGui.QLabel):
 				os.remove(self.parent.parent.settingsFile)
 				#print "Deleted Config"
 				print "exiting..."
-				sys.exit()
+				
+				#sys.exit()
+				
+				os._exit(-1)
 			except:
 				raise
 				
@@ -124,7 +127,39 @@ class IconWidget(QtGui.QWidget):
 		
 		#End Painter
 		painter.end()
+		
+class InputDialog(QtGui.QDialog):
+	'''
+	this is for when you need to get some user input text
+	'''
+	def __init__(self, parent=None, title='user input', label='comment', text=''):
 
+		QtGui.QWidget.__init__(self, parent)
+
+		#--Layout Stuff---------------------------#
+		mainLayout = QtGui.QVBoxLayout()
+
+		layout = QtGui.QHBoxLayout()
+		self.label = QtGui.QLabel()
+		self.label.setText(label)
+		layout.addWidget(self.label)
+
+		self.text = QtGui.QLineEdit(text)
+		layout.addWidget(self.text)
+
+		mainLayout.addLayout(layout)
+
+		#--The Button------------------------------#
+		layout = QtGui.QHBoxLayout()
+		button = QtGui.QPushButton("okay") #string or icon
+		self.connect(button, QtCore.SIGNAL("clicked()"), self.close)
+		layout.addWidget(button)
+
+		mainLayout.addLayout(layout)
+		self.setLayout(mainLayout)
+
+		self.resize(400, 60)
+		self.setWindowTitle(title)
 
 class AccountWidget(QtGui.QWidget):
 	
