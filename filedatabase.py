d8eeb437900ada2171c33851d750fccfdd7363e8
Travis Cunningham <travcunn@umail.iu.edu>
2013-04-18 14:54:23 -0400

    File synchronization version 1 without checking timestamps, milestone

--- filedatabase.py
+++ filedatabase.py
@@ -1,7 +1,8 @@
 from PyQt4 import QtCore, QtGui, QtWebKit, QtSvg
-import datetime, hashlib, os, platform, Queue, sys, time, threading
+import datetime, hashlib, os, platform, Queue, shutil, sys, time, threading, subprocess
 import cPickle as pickle
 
+from StringIO import StringIO
 
 class FileDatabase(object):
 	def __init__(self, parent = None):
@@ -10,6 +11,7 @@ class FileDatabase(object):
 		self.remoteFilesList = []
 		
 		self.remoteFilesDictionary = {}
+		self.remoteFilesDictionaryTime = {}
 		self.localFilesDictionary = {}
 		self.localFilesDictionaryTime = {}
 	
@@ -30,18 +32,66 @@ class FileDatabase(object):
 				
 				print  "[database-indexer]: %s %s" % (discoveredFilePath.replace(syncdirPath,'').encode('utf-8'), filehash.encode('utf-8'))
 		print "[database]: Local Database: Done indexing local files..."
-		
-	def getRemoteFiles(self):
-		self.remoteFilesDictionary[i['path']] = self.hashtask(i['path'])
+
 	
 	def loadRemoteListingFile(self):
-		openPath = self.downloadFile("/.kissync")
-		subprocess.call(('xdg-open', openPath))
+		try:
+			openPath = self.getServerListingFile()
+			openPathTime = self.getServerListingFileTime()
+		except:
+			print "Remote listing does not exist..."
+			openPath = None
+			openPathTime = None
+			self.remoteFilesDictionary = {}
+			self.remoteFilesDictionaryTime = {}
+			#self.generateRemoteListing()
+		else:
+			picklefile = open(openPath, 'rb')
+			self.remoteFilesDictionary = pickle.load(picklefile)
+			picklefile.close()
+			
+			picklefile = open(openPathTime, 'rb')
+			self.remoteFilesDictionaryTime = pickle.load(picklefile)
+			picklefile.close()
+			
+			#subprocess.call(('xdg-open', openPath))
+			#subprocess.call(('xdg-open', openPathTime))
+			
+	def generateRemoteListing(self):
+		tmpLocalPath = self.parent.workingDirectory + "/.kissyncDBtmp"
+		output = open(tmpLocalPath, 'wb')
+		pickle.dump(self.localFilesDictionary, output)
+		output.close()
+		
+		tmpLocalPathTime = self.parent.workingDirectory + "/.kissyncDBtmptime"
+		output = open(tmpLocalPathTime, 'wb')
+		pickle.dump(self.localFilesDictionaryTime, output)
+		output.close()
+		
+		#now upload the pickled files to the server
+		try:
+			fileToUpload = file(tmpLocalPath)
+			self.parent.smartfile.post('/path/data/', file=('/.kissyncDB', fileToUpload))
+			
+			fileToUpload = file(tmpLocalPathTime)
+			self.parent.smartfile.post('/path/data/', file=('/.kissyncDBtime', fileToUpload))
+		except:
+			raise
+			self.generateRemoteListing()
 				
 	def getServerListingFile(self):
-				
-		f = self.parent.parent.parent.smartfile.get('/path/data/', filepath)
-		realPath = self.parent.parent.parent.workingDirectory + filepath
+		filepath = "/.kissyncDB"
+		f = self.parent.smartfile.get('/path/data', filepath)
+		realPath = self.parent.workingDirectory + filepath
+		realPath = realPath.encode("utf-8")
+		with file(realPath, 'wb') as o:
+			shutil.copyfileobj(f, o)
+		return realPath
+		
+	def getServerListingFileTime(self):
+		filepath = "/.kissyncDBtime"
+		f = self.parent.smartfile.get('/path/data/', filepath)
+		realPath = self.parent.workingDirectory + filepath
 		realPath = realPath.encode("utf-8")
 		with file(realPath, 'wb') as o:
 			shutil.copyfileobj(f, o)
@@ -81,6 +131,7 @@ class Synchronizer(object):
 		#print "Compare what files server has against local files:"
 		
 		#files the server that client needs
+		print self.dictDiffer.added()
 		for i in self.dictDiffer.added():
 			self.filesToDownload.append(i)
 			self.downloadQueue.put(i)
@@ -100,15 +151,20 @@ class Synchronizer(object):
 		print self.filesToDownload
 		print "Files to upload:"
 		print self.filesToUpload	
-		"""
+
 		downthread = Downloader(self, self.downloadQueue)
 		downthread.setDaemon(True)
 		downthread.start()
 		
-		upthread = Uploader(self, self.downloadQueue)
+		upthread = Uploader(self, self.uploadQueue)
 		upthread.setDaemon(True)
 		upthread.start()
-		"""
+		
+		self.downloadQueue.join()
+		self.uploadQueue.join()
+		self.parent.database.generateRemoteListing()
+		#self.parent.database.generateRemoteListing()
+
 		
 class Downloader(threading.Thread):
  
@@ -119,6 +175,7 @@ class Downloader(threading.Thread):
  
 	def run(self):
 		while True:
+			print "running downloader..."
 			path = self.queue.get()
 			print path
 			self.downloadFile(path)
@@ -161,8 +218,8 @@ class Uploader(threading.Thread):
 	def uploadFile(self, filepath):
 		filepath = self.syncdirPath + filepath
 		if not (os.path.isdir(filepath)):
-			fileToUpload = file(filepath)
-			self.parent.parent.smartfile.post('/path/data/', file=(filepath.replace(self.syncdirPath,''), fileToUpload))
+			fileToUpload = file(filepath, 'rb')
+			self.parent.parent.smartfile.post('/path/data/', file=(filepath.replace(self.syncdirPath,''), StringIO(fileToUpload)))
 		else:
 			self.parent.parent.smartfile.post('/path/oper/mkdir/', filepath.replace(self.syncdirPath,''))
 			
