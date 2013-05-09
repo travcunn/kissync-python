5e887c82f0a0c23456da4c3b7c6434d6ea04fa7d
Travis Cunningham <travcunn@umail.iu.edu>
2013-05-09 19:25:03 -0400

    Changed the testing script

--- tests.py
+++ tests.py
@@ -6,23 +6,14 @@ import unittest
 class LoginTestCase(unittest.TestCase):
     def test_login(self):
         api = OAuthClient("zGSJpILRq2889Ne2bPBdEmEZLsRHpe", "KOb97irJG84PJ8dtEkoYt2Kqwz3VJa")
-        try:
-            api.get_request_token()
-            api.get_authorization_url()
-        except:
-            oauthtest = False
-        else:
-            oauthtest = True
-        self.assertTrue(oauthtest)
+        api.get_request_token()
+        api.get_authorization_url()
 
 
 class FileDatabaseTest(unittest.TestCase):
     def __init__(self, parent=None):
         super(FileDatabaseTest, self).__init__(parent)
         self.database = FileDatabase(self)
-
-    def test_database_authhash(self):
-        self.assertIsNotNone(self.database.generateAuthHash())
 
     def test_database_indexlocalfiles(self):
         self.assertTrue(self.database.indexLocalFiles())
